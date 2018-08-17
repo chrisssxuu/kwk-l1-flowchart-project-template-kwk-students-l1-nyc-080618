2e6c30ce3845c1d8043a23cd80bc180e07978944
chrisssxuu <chris01.xu@gmail.com>
2018-08-17 14:00:34 +0000

    automatically backed up by learn

---
+++ app/models/hash.rb
@@ -0,0 +1,317 @@
+# artist hash 
+# integer => name, spotify id, insta, twitter
+
+country=
+[
+
+  {
+    "name" => "Maren Morris", 
+    "id" => "6WY7D3jk8zTrHtmkqqo5GI", "insta" => "marenmorris", 
+    "twitter" => "MarenMorris"
+  },
+
+
+{
+  "name" => "Kelsea Ballerini", 
+  "id" => "3RqBeV12Tt7A8xH3zBDDUF", "insta" => "kelseaballerini", "twitter" => "KelseaBallerini"
+},
+
+{
+  "name" => "Miranda Lambert", 
+  "id" => "66lH4jAE7pqPlOlzUKbwA0", "insta" => "mirandalambert", 
+  "twitter" => "mirandalambert"
+},
+
+{
+"name" => "Dixie Chicks", 
+"id" => "25IG9fa7cbdmCIy3OnuH57", 
+"insta" => "dixie_chicks", 
+ "twitter" => "dixiechicks"
+},
+
+
+{
+  "name" => "Dolly Parton", 
+  "id" => "32vWCbZh0xZ4o9gkz4PsEU", 
+  "insta" => "dollyparton", 
+  "twitter" => "DollyParton"
+},
+
+
+{
+  "name" => "Faith Hill", 
+  "id" => "25NQNriVT2YbSW80ILRWJa", 
+  "insta" => "faithhill", 
+   "twitter" => "faithhill"
+},
+
+{
+  "name" => "Trisha Yearwood", 
+  "id" => "3XlIhgydjvC4EniPFZT20j", 
+  "insta" => "trishayearwood",  "twitter" => "trishayearwood"
+},
+
+{
+  "name" => "Lauren Alaina", 
+  "id" => "1v3tdpIdBSW14rHUfiEVOv", 
+  "insta" => "laurenalaina",  
+  "twitter" => "Lauren_Alaina"
+},
+
+{
+  "name" => "Shania Twain", 
+  "id" => "5e4Dhzv426EvQe3aDb64jL", 
+  "insta" => "shaniatwain", 
+   "twitter" => "ShaniaTwain"
+},
+
+{
+  "name" => "RaeLynn", 
+  "id" => "5dMnvpYEfXvSexjwnSRH5n", 
+  "insta" => "raelynnofficial", 
+   "twitter" => "raelynn"
+},
+
+{
+  "name" => "Jillian Jacqueline", 
+  "id" => "5GDZ6xhBwk7Yja97CFLmV7", 
+  "insta" => "jillianjacqeline",  "twitter" => "jillianjmusic"
+},
+
+{
+  "name" => "Sara Evans", 
+  "id" => "7qvsLYsYP0MHD7jkdv6DAG", 
+  "insta" => "saraevansmusic",  "twitter" => "saraevansmusic"
+},
+
+{
+  "name" => "Carrie Underwood", 
+  "id" => "4xFUf1FHVy696Q1JQZMTRj", 
+  "insta" => "carrieunderwood",  "twitter" => "carrieunderwood"
+},
+
+]
+
+pop=
+[
+{
+  "name" => "Sia", 
+  "id" => "5WUlDfRSoLAfcVSX1WnrxN", 
+  "insta" => "siamusic",  
+  "twitter" => "Sia"
+},
+
+{
+  "name"=> "Ingrid Michaelson", 
+  "id" => "2vm8GdHyrJh2O2MfbQFYG0", 
+  "insta" => "ingridmichaelson",  "twitter" => "ingridmusic"
+},
+
+{
+  "name"=> "Lorde", 
+  "id" => "163tK9Wjr9P9DmM0AVK7lm", 
+  "insta" => "lordemusic", 
+   "twitter" => "lorde"
+},
+
+{
+  "name"=> "Julia Michaels", 
+  "id" => "0ZED1XzwlLHW4ZaG4lOT6m", "insta" => "juliamichaels",  
+  "twitter" => "juliamichaels"
+},
+
+{
+  "name"=> "Alessia Cara", 
+  "id" => "2wUjUUtkb5lvLKcGKsKqsR", 
+  "insta" => "alessiasmusic",  
+  "twitter" => "alessiacara"
+},
+
+{
+  "name"=> "Anne-Marie", 
+  "id" => "1zNqDE7qDGCsyzJwohVaoX", 
+  "insta" => "annemarie", 
+   "twitter" => "annemarie"
+},
+
+{
+  "name"=> "Emeli Sande", 
+  "id" => "7sfgqEdoeBTjd8lQsPT3Cy", 
+  "insta" => "emelisande", 
+   "twitter" => "emelisande"
+},
+
+{
+  "name"=> "Florence and the Machine", 
+  "id" => "1moxjboGR7GNWYIMWsRjgG", 
+  "insta" => "florenceandthemachine",  "twitter" => "flo_tweet"
+},
+
+{
+  "name"=> "Hayley Kiyoko", 
+  "id" => "3LjhVl7GzYsza1biQjTpaN", 
+  "insta" => "hayleykiyoko",  
+  "twitter" => "HayleyKiyoko"
+},
+
+{
+  "name"=> "Birdy", 
+  "id" => "2WX2uTcsvV5OnS0inACecP", 
+  "insta" => "birdyinstagram", 
+   "twitter" => "birdy"
+},
+
+] 
+
+indie = [
+  {
+    "name"=> "Flint Eastwood", 
+    "id" => "4Gh0D04oCEVbnsx63LXUmT", 
+    "insta" => "flinteastwood", 
+    "twitter" => "flinteastwood"
+  },
+  {
+    "name"=> "Japanese Breakfast", 
+    "id" => "7MoIc5s9KXolCBH1fy9kkw", 
+    "insta" => "jbrekkie", 
+    "twitter" => "Jbrekkie"
+  },
+  {
+    "name"=> "Mitski", 
+    "id" => "1Uvw5JW1C0Z0rAWiwubAjd", 
+    "insta" => "mitskileaks", 
+    "twitter" => "mitskileaks"
+  },
+  {
+    "name"=> "AURORA", 
+    "id" => "1WgXqy2Dd70QQOU7Ay074N", 
+    "insta" => "auroramusic", 
+    "twitter" => "AURORAmusic"
+  },
+  {
+    "name"=> "Taylor Janzen", 
+    "id" => "3rTVTBOBT7NPgXtI5oWyxc", 
+    "insta" => "taylorjanzenn", 
+    "twitter" => "taylorjanzenn"
+  },
+  {
+    "name"=> "Julien Baker", 
+    "id" => "12zbUHbPHL5DGuJtiUfsip", 
+    "insta" => "julienrbaker", 
+    "twitter" => "julienrbaker"
+  },
+  {
+    "name"=> "Gabrielle Aplin", 
+    "id" => "3w6zswp5THsSKYLICUbDTZ", 
+    "insta" => "gabrielleaplin", 
+    "twitter" => "GabrielleAplin"
+  },
+  {
+    "name"=> "Jain", 
+    "id" => "2HHmvvSQ44ePDH7IKVzgK0", 
+    "insta" => "jainmusic", 
+    "twitter" => "Jainmusic"
+  },
+  {
+    "name"=> "The Regrettes", 
+    "id" => "67WNUxmM7y4WzHPAVzBu3E", 
+    "insta" => "theregrettesband", 
+    "twitter" => "regrettesband"
+  },
+  {
+    "name"=> "Maggie Rogers", 
+    "id" => "4NZvixzsSefsNiIqXn0NDe",
+    "insta" => "maggierogers", 
+    "twitter" => "maggierogers"
+  },
+  {
+    "name"=> "St. Vincent", 
+    "id" => "7bcbShaqKdcyjnmv4Ix8j6",
+    "insta" => "st_vincent", 
+    "twitter" => "st_vincent"
+  },
+  {
+    "name"=> "Lily Allen", 
+    "id" => "13saZpZnCDWOI9D4IJhp1f",
+    "insta" => "lilyallen", 
+    "twitter" => "lilyallen"
+  },
+  {
+    "name"=> "First Aid Kit", 
+    "id" => "21egYD1eInY6bGFcniCRT1",
+    "insta" => "firstaidkitband",
+    "twitter" => "FirstAidKitBand"
+  },
+  {
+    "name"=> "Grimes", 
+    "id" => "053q0ukIDRgzwTr4vNSwab", 
+    "insta" => "grimes", 
+    "twitter" => "Grimezsz"
+  },
+  {
+    "name"=> "Banks", 
+    "id" => "2xe8IXgCTpwHE3eA9hTs4n",
+    "insta" => "hernameisbanks", 
+    "twitter" => "hernameisbanks"
+  },
+  {
+    "name"=> "Courntey Barnett", 
+    "id" => "4OOlG5eBXSkSAAEeKjJb5Y",
+    "insta" => "courtneymelba", 
+    "twitter" => "courtneymelba"
+  },
+  {
+    "name"=> "Best Coast",
+    "id" => "5YkBrE0wF8cAlq3GCOw5Eu",
+    "insta" => "best_coast", 
+    "twitter" => "BestCoast"
+  },
+  {
+    "name"=> "MUNA", 
+    "id" => "6xdRb2GypJ7DqnWAI2mHGn",
+    "insta" => "whereismuna", 
+    "twitter" => "whereisMUNA"
+  },
+  {
+    "name"=> "The Aces", 
+    "id" => "2AmfMGi3WZMxqFDHissIAe", 
+    "insta" => "theaces", 
+    "twitter" => "theacesofficial"
+  },
+  {
+    "name"=> "Clairo", 
+    "id" => "3l0CmX0FuQjFxr8SK7Vqag", 
+    "insta" => "clairo", 
+    "twitter" => "clairo"
+  },
+  {
+    "name"=> "Alvvays", 
+    "id" => "3kzwYV3OCB010YfXMF0Avt",
+    "insta" => "alvvaysband", 
+    "twitter" => "alvvaysband"
+  },
+  {
+    "name"=> "Yeah Yeah Yeahs", 
+    "id" => "3TNt4aUIxgfy9aoaft5Jj2",
+    "insta" => "yeahyeahyeahs", 
+    "twitter" => "yyys"
+  },
+  {
+    "name"=> "Frankie Cosmos", 
+    "id" => "0x4xCoWaOFd3WsKarzaxnW",
+    "insta" => "frankiecombos", 
+    "twitter" => "frankiecosmos"
+  },
+  {
+    "name"=> "Tei Shi", 
+    "id" => "1xcMOgFUM1IYZE22YjCvsL", 
+    "insta" => "teishi", 
+    "twitter" => "TeiShi"
+  },
+  {
+    "name"=> "Lana Del Rey", 
+    "id" => "00FQb4jTyendYWaN8pK0wa",
+    "insta" => "lanadelrey", 
+    "twitter" => "lanadelrey"
+  },
+]
